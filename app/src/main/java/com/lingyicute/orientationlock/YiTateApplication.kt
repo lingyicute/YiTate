d4683891f4931088e4a63fd7651d724543bc6319
lingyicute <160479206+lingyicute@users.noreply.github.com>
2025-02-02 20:39:17 +0800

    Update

--- app/src/main/java/com/lingyicute/orientationlock/YiTateApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.lingyicute.orientationlock
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class YiTateApplication : Application() 
--- app/src/main/java/com/lingyicute/orientationlock/data/ExcludedApp.kt
+++
@@ -1,7 +0,0 @@
-package com.lingyicute.orientationlock.data
-
-data class ExcludedApp(
-    val packageName: String,
-    val appName: String,
-    val isExcluded: Boolean = false
-) 
